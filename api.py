e998eb293d788bc6ed5a6ba926d0cf3c5db41ab7
Reginaldo Morais <reginaldo.cmorais@gmail.com>
2020-02-15 01:54:33 -0300

    feat: Implements GET verb to list users

--- api.py
+++ api.py
@@ -1,12 +1,22 @@
 import flask
+import json
+from flask import jsonify
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
+users = json.loads(
+    '[{"id": 1, "name": "Reginaldo Morais"}, {"id": 2, "name": "John Doe"}]')
 
-@app.route('/', methods=['GET'])
-def home():
-    return "Hello World!"
+
+@app.route('/users', methods=['GET'])
+def list():
+    return jsonify(users)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return "<h1>404 - Not Found</h1><p>The resource could not be found.</p>", 404
 
 
 app.run()
