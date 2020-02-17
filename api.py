036f7b8207ffaacf965c4041c301fd7ccd55981a
Reginaldo Morais <reginaldo.cmorais@gmail.com>
2020-02-17 10:44:36 -0300

    feat: Implements POST verb to save a user

--- api.py
+++ api.py
@@ -1,25 +1,57 @@
 import flask
 import json
-from flask import jsonify
+from flask import jsonify, request
+from functools import wraps
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
+
 users = json.loads(
     '[{"id": 1, "name": "Reginaldo Morais"}, {"id": 2, "name": "John Doe"}]')
+
+
+def validate_json(*expected_args):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            json_ob = request.get_json()
+            for expected_arg in expected_args:
+                if expected_arg not in json_ob or json_ob.get(expected_arg) is None:
+                    return bad_request(f'Field [{expected_arg}] is required')
+            return func(*args, **kwargs)
+        return wrapper
+    return decorator
 
 
 @app.route('/users', methods=['GET'])
 def list():
     return jsonify(users)
 
+
 @app.route('/users/<int:id>', methods=['GET'])
 def get(id):
     for user in users:
         if user['id'] == id:
-            return jsonify(user);
+            return jsonify(user)
     
-    return page_not_found("sss");
+    return page_not_found()
+
+
+@app.route('/users', methods=['POST'])
+@validate_json("id", "name")
+def post():
+    data = request.get_json()
+    users.append(data)
+    
+    return jsonify(users)
+
+
+@app.errorhandler(400)
+def bad_request(e=None):
+    if e:
+        print(f'Error: {e}');
+    return jsonify({"Error": e}), 400
 
 
 @app.errorhandler(404)
